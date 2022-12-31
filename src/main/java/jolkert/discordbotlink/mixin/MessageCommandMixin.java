a3d9b69fb4b8dad0c8dc5c23e4e7f1a68cae51ce
Jolkert <jolkertt@gmail.com>
2022-12-30 23:56:33 -0500

    im trying to update this to 1.19.3 but i suck so please hold lmao

--- src/main/java/jolkert/discordbotlink/mixin/MessageCommandMixin.java
+++ src/main/java/jolkert/discordbotlink/mixin/MessageCommandMixin.java
@@ -2,12 +2,14 @@ package jolkert.discordbotlink.mixin;
 
 import jolkert.discordbotlink.DiscordBotLink;
 import net.minecraft.command.argument.MessageArgumentType;
+import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.command.MessageCommand;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
@@ -18,7 +20,7 @@ import java.util.List;
 public class MessageCommandMixin
 {
 	@Inject(at = @At("TAIL"), method = "execute")//, locals = LocalCapture.CAPTURE_FAILSOFT)
-	private static void logMessageToConsole(ServerCommandSource source, Collection<ServerPlayerEntity> targets, MessageArgumentType.SignedMessage signedMessage, CallbackInfoReturnable<Integer> cir)
+	private static void logMessageToConsole(ServerCommandSource source, Collection<ServerPlayerEntity> targets, SignedMessage message, CallbackInfo ci)
 	{
 		String sender = source.getName();
 		if (sender == null || sender.isEmpty())
@@ -26,6 +28,6 @@ public class MessageCommandMixin
 		
 		List<ServerPlayerEntity> players = new ArrayList<>(targets);
 		for (ServerPlayerEntity player : players)
-			DiscordBotLink.Logger.info("Whisper: [" + sender + " -> " + player.getName().getString() + "] " + signedMessage.plain());
+			DiscordBotLink.Logger.info("Whisper: [" + sender + " -> " + player.getName().getString() + "] " + message.getContent().getString());
 	}
 }
--- src/main/java/jolkert/discordbotlink/mixin/PlayerManagerMixin.java
+++ src/main/java/jolkert/discordbotlink/mixin/PlayerManagerMixin.java
@@ -2,35 +2,66 @@ package jolkert.discordbotlink.mixin;
 
 import jolkert.discordbotlink.DiscordBotLink;
 import jolkert.discordbotlink.data.UserData;
-import net.minecraft.network.message.MessageSender;
+import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.*;
-import net.minecraft.util.Formatting;
+import org.apache.logging.log4j.core.jmx.Server;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin
 {
 	@ModifyArg(
-		method = "broadcast(Lnet/minecraft/network/message/SignedMessage;Ljava/util/function/Function;Lnet/minecraft/network/message/MessageSender;Lnet/minecraft/util/registry/RegistryKey;)V",
-		at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;sendChatMessage(Lnet/minecraft/network/message/SignedMessage;Lnet/minecraft/network/message/MessageSender;Lnet/minecraft/util/registry/RegistryKey;)V"))
-	public MessageSender addHoverTextToSender(MessageSender sender)
+			method = "broadcast(Lnet/minecraft/network/message/SignedMessage;Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/network/message/MessageType$Parameters;)V",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/network/message/SignedMessage;Ljava/util/function/Predicate;Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/network/message/MessageType$Parameters;)V"))
+	public SignedMessage addHoverTextToSender(SignedMessage message)
 	{
-		
-		UserData data = DiscordBotLink.Bot.getUserData().getUser(sender.uuid());
-		
-		if (data != null)
+		UserData data = DiscordBotLink.Bot.getUserData().getUser(message.getSender());
+		// if (data == null)
+		// return message;
+
+		// int topColor = data.getRoleInfo().getTopColor();
+		String content = message.getContent().getString();
+		String[] isolatedContent = isolateUsername(content);
+
+		MutableText text = MutableText.of(LiteralTextContent.EMPTY);
+
+        System.out.println("{" + String.join(", ", isolatedContent) + "}");
+		for (int i = 0; i < isolatedContent.length; i++)
 		{
-			int topColor = data.getRoleInfo().getTopColor();
-			
-			return new MessageSender(sender.uuid(), MutableText.of(new LiteralTextContent(sender.name().getString())).setStyle(
-					Style.EMPTY.withColor(TextColor.fromRgb(Math.min(topColor, 0xffffff)))
-							   .withHoverEvent(HoverEvent.Action.SHOW_TEXT.buildHoverEvent(Text.of(data.getHoverText())))
-							   .withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/msg " + sender.name().getString()))));
+			if (i != 1)
+				text.append(isolatedContent[i]);
+			else
+			{
+				Style style = Style.EMPTY.withColor(0xff0000)
+						.withHoverEvent(HoverEvent.Action.SHOW_TEXT.buildHoverEvent(Text.of(data.getHoverText())))
+						.withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/msg" + isolatedContent[i]));
+
+				text.append(MutableText.of(new LiteralTextContent(isolatedContent[i])).setStyle(style));
+			}
 		}
-		else
-			return sender;
+
+		return message.withUnsignedContent(text);
+	}
+
+	private static String[] isolateUsername(String str)
+	{
+		String[] arr = new String[3];
+
+		arr[0] = str.substring(0, str.indexOf('<'));
+		arr[1] = str.substring(str.indexOf('<') + 1, str.indexOf('>'));
+		arr[2] = str.substring(str.indexOf('>') + 1);
+
+		return arr;
 	}
 }
