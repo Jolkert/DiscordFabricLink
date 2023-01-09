a60774a0899dc25823988e0463fb7cc5d2fd9f5a
Jolkert <jolkertt@gmail.com>
2023-01-08 20:19:54 -0500

    Parrot interaction improved and is success now :)

--- src/main/java/jolkert/discordbotlink/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/jolkert/discordbotlink/mixin/ServerPlayerInteractionManagerMixin.java
@@ -7,7 +7,9 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,10 +17,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ServerPlayerInteractionManager.class)
 public class ServerPlayerInteractionManagerMixin
 {
-	@Inject(method = "interactBlock", at = @At("HEAD"))
-	public void dropOnEmpty(ServerPlayerEntity player, World world, ItemStack stack, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir)
+	@Shadow @Final protected ServerPlayerEntity player;
+
+	@Inject(method = "interactBlock", at = @At("RETURN"), cancellable = true)
+	public void dropShoulders(ServerPlayerEntity player, World world, ItemStack stack, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> callbackInfo)
 	{
-		if (player.isSneaking() && player.getMainHandStack().isEmpty())
+		if (!callbackInfo.getReturnValue().isAccepted()
+				&& hasShoulderEntity(player)
+				&& player.isSneaking()
+				&& player.getMainHandStack().isEmpty())
+		{
 			((PlayerInvoker) player).invokeDropShoulderEntities();
+			callbackInfo.setReturnValue(ActionResult.SUCCESS);
+			callbackInfo.cancel();
+		}
+	}
+
+	private static boolean hasShoulderEntity(ServerPlayerEntity player)
+	{
+		return !(player.getShoulderEntityLeft().isEmpty() && player.getShoulderEntityRight().isEmpty());
 	}
 }
